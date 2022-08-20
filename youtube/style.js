527ca9f4339d24b19a79e6c4d4a19e36b9190964
tjdgns8439 <tjdgns8439@inha.edu>
2022-08-20 21:51:36 +0900

    파일 분리
    youtube.js와 스타일 파일을 App.js에서 분리함

---
+++ youtube/style.js
@@ -0,0 +1,49 @@
+import { StyleSheet,Dimensions } from "react-native";
+
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SCREEN_HEIGHT = Dimensions.get('window').height;
+
+export const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor : "white",
+      },
+      city:{
+        marginTop:30,
+        marginBottom:-30,
+        height:100,
+        justifyContent:"center",
+        alignItems:"center",
+        flexDirection: 'row'
+      },
+      cityName:{
+        fontSize:48,
+        fontWeight:"500",
+      },
+      weather:{
+      },
+      day:{
+        width: SCREEN_WIDTH,
+        alignItems:"center",
+      },
+      temp:{
+        marginTop: 30,
+        fontSize: 20,
+      },
+      description:{
+        marginTop: -30,
+        fontSize: 60,
+      },
+      tinyText:{
+        fontSize:20,
+      },
+      back:{
+        width: SCREEN_WIDTH,
+        height : SCREEN_HEIGHT/3,
+      },
+      logo:{
+        width: 50,
+        height : 50,
+      },
+})
